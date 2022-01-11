1ad81068acd7425fcdf6c1f9220529685177ebf1
Just-Riko <kitchakpavlonew@gmail.com>
2022-01-11 20:20:00 +0200

    Update js.js

--- js.js
+++ js.js
@@ -24,33 +24,9 @@ some()
 
 let time = 600;
 
-for (i of mod) {
-    if (i.selector == '.dot1') {
-        for (let i = 0; i < 4; i++){
-            setTimeout(()=>$dot[0].children[i].classList.add('active'),time)
-            time+=200
-        }
-    }
-    if (i.selector == '.dot2') {
-        for (let i = 0; i < 4; i++){
-            setTimeout(()=>$dot[1].children[i].classList.add('active'),time)
-            time+=200
-        }
-    }
-    if (i.selector == '.dot3') {
-        for (let i = 0; i < 3; i++){
-            setTimeout(()=>$dot[2].children[i].classList.add('active'),time)
-            time+=200
-        }
-    }
-    if (i.selector == '.dot4') {
-        setTimeout(()=>$dot[3].children[0].classList.add('active'),time)
+for (let x = 0; x < mod.length; x++) {
+    for (let i = 0; i < mod[x].active; i++){
+        setTimeout(()=>$dot[x].children[i].classList.add('active'),time)
         time+=200
-    }
-    if (i.selector == '.dot5'){
-        for (let i = 0; i < 3; i++){
-            setTimeout(()=>$dot[4].children[i].classList.add('active'),time)
-            time+=200
-        }
     }
 }
